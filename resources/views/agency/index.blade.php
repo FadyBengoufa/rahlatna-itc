f69f4be66b2f1d923e73eea0fea38fdd3661429f
Fady Bengoufa <fady.bengoufa@gmail.com>
2025-02-12 21:39:37 +0100

    small fix

--- resources/views/agency/index.blade.php
+++ resources/views/agency/index.blade.php
@@ -6,8 +6,8 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <a href="{{ route('trips.create') }}">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 px-4">
+            <a href="{{ route('trips.create') }}" class="flex md:justify-end mb-4">
                 <button 
                     type="button"
                     class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 flex items-center">
